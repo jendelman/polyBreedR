032a90ccc18858c9ecaee8b86a1bfa96b884ccfc
Jeffrey Endelman <endelman@wisc.edu>
2020-07-26 14:02:49 -0500

    v0.11

--- R/GvsA.R
+++ R/GvsA.R
@@ -10,18 +10,29 @@
 #' @param filename Name of PDF file to save the results (optional for one individual)
 #' @param thresh.G Threshold above which names are displayed (default Inf)
 #' @param thresh.A Threshold above which names are displayed (default 0.5)
+#' @param Gmax Upper limit for y-axis for plotting. If NULL, maximum value in G is used.
+#' @param Amax Upper limit for x-axis for plotting. If NULL, maximum value in A is used.
 #' 
 #' @import ggplot2
 #' @importFrom ggrepel geom_text_repel
 #' @importFrom grDevices dev.off pdf
 #' @export
-GvsA <- function(parentage,G,A,filename=NULL,thresh.G=Inf,thresh.A=0.5) {
+GvsA <- function(parentage,G,A,filename=NULL,thresh.G=Inf,thresh.A=0.5,Gmax=NULL,Amax=NULL) {
   
   id <- as.character(parentage[,1])
   n <- length(id)
   if (n > 1 & is.null(filename)) {
     stop("Filename is required.")
   }
+  diag(A) <- NA
+  diag(G) <- NA
+  if (is.null(Gmax)) {
+    Gmax <- max(G,na.rm=T)  
+  }
+  if (is.null(Amax)) {
+    Amax <- max(A,na.rm=T)  
+  }
+  Gmin <- min(G,na.rm=T)
   if (!is.null(filename)) {
     filename <- sub(pattern=".pdf",replacement="",filename)
     filename <- sub(pattern=".PDF",replacement="",filename)
@@ -38,7 +49,7 @@ GvsA <- function(parentage,G,A,filename=NULL,thresh.G=Inf,thresh.A=0.5) {
     plotme <- data.frame(id=others[-z],x=A[j,others][-z],y=G[k,others][-z],stringsAsFactors = F)
     plotme$id <- ifelse(plotme$x >= thresh.A | plotme$y >= thresh.G,plotme$id,"")
     ttl <- paste(id[i],paste(as.character(parentage[i,2]),as.character(parentage[i,3]),sep=" / "),sep=" = ")
-    p <- ggplot(data=plotme,aes(x=x,y=y)) + geom_point() + geom_text_repel(aes(label=id),show.legend=FALSE,segment.color="red",colour="red") + theme_bw() + ggtitle(ttl) + xlab("A") + ylab("G")
+    p <- ggplot(data=plotme,aes(x=x,y=y)) + geom_point() + geom_text_repel(aes(label=id),show.legend=FALSE,segment.color="red",colour="red") + theme_bw() + ggtitle(ttl) + xlab("A") + ylab("G") + xlim(c(0,Amax)) + ylim(c(Gmin,Gmax))
     if (!is.null(filename)){print(p)}
   }
   if (!is.null(filename)) {
