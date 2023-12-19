2b4cc8a8b0e39338335ae620567b528647afc60c
jendelman <endelman@wisc.edu>
2023-12-19 13:52:39 -0600

    new impute_L2H RF

--- R/GT2DS.R
+++ R/GT2DS.R
@@ -1,24 +1,46 @@
-#' Convert GT to DS
+#' Convert GT to ALT allele dosage (DS)
 #' 
-#' Convert GT to DS
+#' Convert GT to ALT allele dosage (DS)
+#' 
+#' If \code{diploidize} is TRUE, data are recoded as a diploid {0,1,2}.
 #' 
 #' @param GT GT string
 #' @param diploidize TRUE/FALSE
+#' @param n.core number of cores
 #' 
-#' @return numeric DS
+#' @return integer data with same dimensions as GT
 #' @export
+#' @importFrom parallel makeCluster stopCluster parApply clusterExport
 
-GT2DS <- function(GT,diploidize=FALSE) {
-  #check for missing data
-  missing <- which(GT==".")
-  ploidy <- sapply(gregexpr("/",GT,fixed=T),length)+1
-  if (length(missing) > 0)
-	  ploidy[missing] <- NA
-  options(warn = -1)
-  geno <- sapply(strsplit(as.character(GT),split="/",fixed=T),function(x){sum(as.integer(x))})
-  options(warn = 0)
-  if (diploidize) {
-    geno <- ifelse(geno==0,0,ifelse(geno==ploidy,2,1))
+GT2DS <- function(GT, diploidize=FALSE, n.core=1) {
+  
+  G2D <- function(GT, diploidize) {
+    missing <- which(GT==".")
+    ploidy <- sapply(gregexpr("/",GT,fixed=T),length)+1
+    if (length(missing) > 0)
+      ploidy[missing] <- NA
+    options(warn = -1)
+    geno <- sapply(strsplit(as.character(GT),split="/",fixed=T),function(x){sum(as.integer(x))})
+    options(warn = 0)
+    if (diploidize) {
+      geno <- ifelse(geno==0,0,ifelse(geno==ploidy,2,1))
+    }
+    return(geno)
+  }
+  
+  if (inherits(GT,"matrix")) {
+    if (n.core > 1) {
+      cl <- makeCluster(n.core)
+      clusterExport(cl=cl,varlist=NULL)
+      geno <- parApply(cl=cl,X=GT,MARGIN=2,FUN=G2D,diploidize=diploidize)
+      stopCluster(cl)
+    } else {
+      geno <- apply(GT,2,G2D,diploidize=diploidize)
+    }
+    dimnames(geno) <- dimnames(GT)
+  } else {
+    geno <- G2D(GT,diploidize=diploidize)
+    names(geno) <- names(GT)
   }
   return(geno)
 }
--- R/impute_L2H.R
+++ R/impute_L2H.R
@@ -2,9 +2,9 @@
 #' 
 #' Impute from low to high density markers
 #' 
-#' Two methods are implemented: Random Forest (RF) and PolyOrigin (PO)
+#' Two methods are available: Random Forest (RF) and PolyOrigin (PO)
 #' 
-#' With RF, any missing data are imputed separately for each file, using the population mean (DS) or mode (GT) for each marker. For RF, argument \code{params} recognizes the following parameters: "format","n.tree","n.mark". Format can have values "GT" or "DS" (default = "GT"), n.tree is the number of trees (default = 100), and n.mark is the number of markers to use as predictors (default = 100), chosen based on minimum distance to the target. Classification trees are used for GT and regression trees for DS. Any markers in low.file that are not present in the high.file or with an identical position are discarded.
+#' With RF, any missing data are imputed separately for each file, using the population mean (DS) or mode (GT) for each marker. For RF, argument \code{params} recognizes the following parameters: "format","n.tree","n.mark","exclude". \code{format} can have values "GT" or "DS" (default = "GT"). \code{n.tree} is the number of trees (default = 100). \code{n.mark} is the number of markers to use as predictors (default = 100), chosen based on minimum distance to the target. \code{exclude} is a vector of sample names to exclude from the high density file, which is useful for cross-validation. Classification trees are used for GT and regression trees for DS. Both VCF and CSV files are recognized for the input files with method RF, based on the file extension. For CSV files, the first three columns should be marker, chrom, pos, and parameter \code{format} still controls whether classification or regression trees are used. 
 #' 
 #' For method PO, the high density file is the phased parents file from PolyOrigin, which has columns marker, chromosome, position (cM), followed by the phased parental genotypes. Argument \code{params} recognized the following parameters: "ped.file" (default is "ped.csv"), "map.file" (default is NULL), and "format", which can take values "GT" (default) or "AD". The map positions in the high density input file from PO are in cM. To export the imputed data with bp positions, provide name of "map.file" with this information. You must enable command line calls to Julia for the PO method.
 #' 
@@ -15,10 +15,10 @@
 #' @param params list of parameters (see Details)
 #' @param n.core multicore processing 
 #' 
-#' @return for RF method, vector of mean OOB error (vs. number of trees)
+#' @return for RF method, matrix of OOB error with dimensions markers x trees
 #' 
 #' @import vcfR
-#' @importFrom stats cov sd
+#' @importFrom stats cov sd na.omit
 #' @importFrom parallel makeCluster stopCluster parApply clusterExport
 #' @importFrom randomForest randomForest
 #' @importFrom data.table fread fwrite
@@ -43,25 +43,24 @@ impute_L2H <- function(high.file, low.file, out.file, method, params=list(),
     }
     return(x)
   }
-  impute.RF <- function(i,geno.train,geno.test,pred.ix,params) {
-    y <- geno.train[,i]
-    ntest <- nrow(geno.test)
-    prix <- as.character(na.omit(pred.ix[i,]))
+  impute.RF <- function(data, geno, train.id, pred.id, params) {
+    y <- data$y
+    ntest <- nrow(geno) - length(train.id)
     if (params$format=="GT") {
       if (length(unique(y))==1) {
         return(list(pred=rep(y[1],ntest), error=rep(0,params$n.tree)))
       } else {
-        ans <- randomForest(y=factor(y), x=geno.train[,prix],
-                            xtest=geno.test[,prix],
+        ans <- randomForest(y=factor(y), x=geno[train.id,data$pred,drop=FALSE],
+                            xtest=geno[pred.id,data$pred,drop=FALSE],
                             ntree=params$n.tree)
-        return(list(pred=as.character(ans$test$predicted), error=ans$err.rate[,1]))
+        return(list(pred=ans$test$predicted, error=ans$err.rate[,1]))
       }
     } else {
       if (sd(y)==0) {
         return(list(pred=rep(y[1],ntest), error=rep(0,params$n.tree)))
       } else {
-        ans <- suppressWarnings(randomForest(y=y, x=geno.train[,prix],
-                                             xtest=geno.test[,prix],
+        ans <- suppressWarnings(randomForest(y=y, x=geno[train.id,data$pred,drop=FALSE],
+                                             xtest=geno[pred.id,data$pred,drop=FALSE],
                                              ntree=params$n.tree))
         return(list(pred=ans$test$predicted, error=ans$mse))
       }
@@ -77,90 +76,128 @@ impute_L2H <- function(high.file, low.file, out.file, method, params=list(),
     if (!("n.mark" %in% np))
       params$n.mark <- 100
     
-    data <- read.vcfR(file=high.file,verbose=FALSE)
-    map1 <- data.frame(marker=data@fix[,"ID"], 
-                       chrom=data@fix[,"CHROM"], 
-                       pos=as.integer(data@fix[,"POS"]))
-    map1$marker[which(is.na(map1$marker) | map1$marker==".")] <- paste(map1$chrom,map1$pos,sep="_")
-    m1 <- nrow(map1)
-    cp1 <- paste(map1$chrom, map1$pos, sep="_")
+    vcf1 <- length(grep("VCF",toupper(high.file),fixed=T)) > 0
+    if (vcf1) {
+      high <- read.vcfR(file=high.file,verbose=FALSE)
+      
+      fields <- vcf_field_names(high, tag = "FORMAT")$ID
+      if (!(params$format %in% fields))
+        stop("Invalid FORMAT")
     
-    fields <- vcf_field_names(data, tag = "FORMAT")$ID
-    if (!(params$format %in% fields))
-      stop("Invalid FORMAT")
-    
-    if (params$format=="GT") {
-      geno1 <- apply(apply(extract.gt(data,element="GT"), 2, impute.mode),1,GT2DS) #transposes
-      rownames(geno1) <- colnames(data@gt)[-1]
+      if (params$format=="GT") {
+        #geno1 is oriented id x marker
+        geno1 <- t(GT2DS(apply(extract.gt(high,element="GT"), 2, impute.mode),
+                     n.core=n.core))
+      } else {
+        geno1 <- t(apply(extract.gt(high,element="DS",as.numeric=T), 2, impute.mean))
+      }
+      
+      map1 <- data.frame(marker=high@fix[,"ID"], 
+                         chrom=high@fix[,"CHROM"], 
+                         pos=as.integer(high@fix[,"POS"]))
+      map1$marker[which(is.na(map1$marker) | map1$marker==".")] <- paste(map1$chrom,map1$pos,sep="_")
+      
     } else {
-      geno1 <- t(apply(extract.gt(data,element="DS",as.numeric=T), 2, impute.mean))
+      high <- fread(high.file,check.names=F) 
+      map1 <- high[,1:3]
+      colnames(map1) <- c("marker","chrom","pos")
+      geno1 <- t(as.matrix(high[,-(1:3)]))
+      if (params$format=="GT")
+        geno1 <- apply(geno1,2,as.integer)
+      dimnames(geno1) <- list(colnames(high)[-(1:3)],map1$marker)
     }
-    colnames(geno1) <- data@fix[,"ID"]
-  
-    data <- read.vcfR(file=low.file,verbose=FALSE)
-    map2 <- data.frame(marker=data@fix[,"ID"], 
-                       chrom=data@fix[,"CHROM"], 
-                       pos=as.integer(data@fix[,"POS"]))
-    map2$marker[which(is.na(map2$marker) | map2$marker==".")] <- paste(map2$chrom,map2$pos,sep="_")
+    if ("exclude" %in% np) {
+      geno1 <- geno1[setdiff(colnames(geno1),params$exclude),]
+    }
     
-    fields <- vcf_field_names(data, tag = "FORMAT")$ID
-    if (!(params$format %in% fields))
-      stop("Invalid FORMAT")
-    
-    if (params$format=="GT") {
-      geno2 <- apply(apply(extract.gt(data,element="GT"), 2, impute.mode),1,GT2DS)
-      rownames(geno2) <- colnames(data@gt)[-1]
+    vcf2 <- length(grep("VCF",toupper(low.file),fixed=T)) > 0
+    if (vcf2) {
+      low <- read.vcfR(file=low.file,verbose=FALSE)
+      
+      fields <- vcf_field_names(low, tag = "FORMAT")$ID
+      if (!(params$format %in% fields))
+        stop("Invalid FORMAT")
+      
+      if (params$format=="GT") {
+        geno2 <- t(GT2DS(apply(extract.gt(low,element="GT"), 2, impute.mode),
+                         n.core=n.core))
+      } else {
+        geno2 <- t(apply(extract.gt(low,element="DS",as.numeric=T), 2, impute.mean))
+      }
+      
+      map2 <- data.frame(marker=low@fix[,"ID"], 
+                         chrom=low@fix[,"CHROM"], 
+                         pos=as.integer(low@fix[,"POS"]))
+      map2$marker[which(is.na(map2$marker) | map2$marker==".")] <- paste(map2$chrom,map2$pos,sep="_")
+      
     } else {
-      geno2 <- t(apply(extract.gt(data,element="DS",as.numeric=T), 2, impute.mean))
+      low <- fread(low.file,check.names=F) 
+      map2 <- low[,1:3]
+      colnames(map2) <- c("marker","chrom","pos")
+      geno2 <- t(as.matrix(low[,-(1:3)]))
+      if (params$format=="GT")
+        geno2 <- apply(geno2,2,as.integer)
+      dimnames(geno2) <- list(colnames(low)[-(1:3)],map2$marker)
     }
-    colnames(geno2) <- data@fix[,"ID"]
     
-    map2 <- map2[map2$marker %in% map1$marker,]
-    m2 <- nrow(map2) 
-    if (m2==0)
-      stop("There are no common markers.")
-    geno2 <- geno2[,map2$marker]
+    train.id <- intersect(rownames(geno1),rownames(geno2))
+    pred.id <- setdiff(rownames(geno2),train.id)
+    if (length(pred.id)==0)
+      stop("All low-density individuals in high-density file.")
+      
+    nid <- length(train.id)
+    cat(sub("Q",nid,"Training Set N=Q\n"))
+    if (nid < 5) {
+      stop("Insufficient Training Set")
+    }
+    geno1 <- geno1[train.id,]
     
-    map.imp <- map1[!(map1$marker %in% map2$marker),]
-    mi <- nrow(map.imp)
-    if (mi==0)
-      stop("No markers to impute.")
+    m1 <- ncol(geno1)
+    m2 <- ncol(geno2)
     
     if (n.core > 1) {
       cl <- makeCluster(n.core)
       clusterExport(cl=cl,varlist=NULL)
     }
     
-    pred.ix <- matrix(as.character(NA),nrow=mi,ncol=params$n.mark)
-    rownames(pred.ix) <- map.imp$marker
-    for (i in 1:mi) {
-      ix <- which(map2$chrom==map.imp$chrom[i])
+    data <- vector("list",m1)
+    names(data) <- map1$marker
+    for (i in 1:m1) {
+      ix <- which(map2$chrom==map1$chrom[i])
       zz <- min(params$n.mark,length(ix))
-      pred.ix[i,1:zz] <- map2$marker[ix[order(abs(map2$pos[ix]-map.imp$pos[i]))[1:zz]]]
+      data[[i]] <- list(y=geno1[,i],
+                   pred=map2$marker[ix[order(abs(map2$pos[ix]-map1$pos[i]))[1:zz]]])
     }
       
     if (n.core > 1) {
-      tmp <- parApply(cl,array(map.imp$marker),MARGIN=1,FUN=impute.RF,
-                             geno.train=geno1, geno.test=geno2, pred.ix=pred.ix, params=params)
+      tmp <- parLapply(cl, X=data, fun=impute.RF, geno=geno2, train.id, pred.id, params)
     } else {
-      tmp <- apply(array(map.imp$marker),MARGIN=1,FUN=impute.RF,
-                          geno.train=geno1, geno.test=geno2, pred.ix=pred.ix, params=params)
+      tmp <- lapply(data, FUN=impute.RF, geno=geno2, train.id, pred.id, params)
     }
-    geno.imp <- matrix(sapply(tmp,"[[",1),nrow=mi,byrow = T)
-    rownames(geno.imp) <- map.imp$marker
-    error <- apply(matrix(sapply(tmp,"[[",2),nrow=mi,byrow = T),2,mean)
+    geno.imp <- matrix(sapply(tmp,"[[",1),nrow=m1,byrow = T)
+    dimnames(geno.imp) <- list(map1$marker,pred.id)
+    geno.out <- cbind(t(geno1[train.id,]),geno.imp)[,rownames(geno2)]
+    error <- matrix(sapply(tmp,"[[",2),nrow=m1,byrow = T)
     
     if (n.core > 1) {
       stopCluster(cl)
     }
-   
-    geno.out <- rbind(t(geno2),geno.imp)
-    map.out <- rbind(map2,map.imp)
-    map.out <- map.out[order(map.out$chrom,map.out$pos),]
-    geno.out <- geno.out[map.out$marker,]
     
-    fwrite(cbind(map.out,geno.out),file=out.file,row.names=F)
+    # if (vcf1) {
+    #   GT <- apply(geno.out,c(1,2),make_GT,ploidy=ploidy)
+    #   info <- make_info(cbind(NS=ncol(geno.out),
+    #                           OOB=error[,params$n.tree]))
+    #   fixed <- cbind(high@fixed[,1:5],
+    #                  rep(".",m1),rep(".",m1),info)
+    #   colnames(fixed) <- c("CHROM","POS","ID","REF","ALT","QUAL","FILTER","INFO")
+    #   write_vcf(filename=out.file,
+    #             fixed=fixed,
+    #             geno=list(GT=GT,AD=AD,DP=DP))
+    # } else {
+    fwrite(cbind(map1,geno.out),file=out.file,row.names=F)
+    
     return(error)
+    
   } else {
     #method PO
     np <- names(params)
